a47815eb52f4fdb524f99050c71e4ad812778d66
ashutoshbadure <ashutoshbadure@gmail.com>
2020-12-27 08:34:25 +0530

    Create Phonenumber

---
+++ Create-Phonenumber/solution.js
@@ -0,0 +1,7 @@
+function createPhoneNumber(numbers) {
+  let phoneNumber = "(xxx) xxx-xxxx";
+  numbers.forEach((number) => {
+    phoneNumber = phoneNumber.replace("x", number);
+  });
+  return phoneNumber;
+}
